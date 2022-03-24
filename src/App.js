2bbd4b61327f314be14e4248ae24f4180c9560fc
ksy70029 <80887385+ksy70029@users.noreply.github.com>
2022-03-24 18:43:00 +0900

    #7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,44 +1,32 @@
-import { useEffect, useState } from 'react';
-
-function Hello() {
-  useEffect(() => {
-    console.log("created :)");
-    return () => console.log("destroyed :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
-
-// function Hello() {
-//   function byFn(){
-//     console.log("destroyed :(");
-//   }
-//   function hiFn(){
-//     console.log("created :)");
-//     return byFn;
-//   }
-//   useEffect(hiFn, []);
-//   return <h1>Hello</h1>;
-// }
-
-// function Hello(){
-//   useEffect(function () {
-//     console.log("hi :)");
-//     return function () {
-//       console.log("bye :(");
-//     };
-//   }, []);
-//   return <h1>Hello</h1>;
-// }
+import { useEffect, useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev)=> !prev);
-  return (
-    <div>
-      {showing ? <Hello/> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
+	const [toDo, setToDo] = useState("");
+	const [toDos, setToDos] = useState([]);
+	const onChange = (event) => setToDo(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if (toDo === "") {
+			return;
+		}
+		setToDos((currentArray) => [toDo, ...currentArray]);
+		setToDo("");
+	};
+	console.log(toDos);
+	return (
+		<div>
+			<h1>My toDos ({toDos.length})</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					onChange={onChange}
+					value={toDo}
+					type="text"
+					placeholder="Write your to do..."
+				/>
+				<button>Add To Do</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
